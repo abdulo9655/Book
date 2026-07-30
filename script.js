8825bf28c52ab48676345ad1d3e4cf2b4f0978e0
Aum13 <aumboonpakin@gmail.com>
2026-07-30 14:27:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-document.body.addEventListener("click",()=>{
+// หน้าแรก
+if (document.getElementById("idle")) {
+    document.body.addEventListener("click", () => {
+        window.location.href = "form.html";
+    });
+}
 
-window.location.href="form.html";
+// หน้ากรอกข้อมูล
+const sendBtn = document.getElementById("sendBtn");
 
-});
+if (sendBtn) {
+    sendBtn.addEventListener("click", () => {
+
+        const book = document.getElementById("bookID").value.trim();
+        const email = document.getElementById("email").value.trim();
+
+        if (!book || !email) {
+            alert("กรุณากรอกข้อมูลให้ครบ");
+            return;
+        }
+
+        // พาร์ตต่อไปจะส่งข้อมูลไป Google Sheets ตรงนี้
+        window.location.href = "receive.html";
+    });
+}
